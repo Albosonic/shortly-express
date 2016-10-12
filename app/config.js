b73f45fb8b5e888be6be9bb5963c6d2f2df628c0
El_Gato <albertomadueno@gmail.com>
2016-10-12 12:21:35 -0700

    deployment

--- app/config.js
+++ app/config.js
@@ -54,18 +54,4 @@ db.knex.schema.hasTable('users').then(function(exists) {
   }
 });
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function(user) {
-//       user.increments('id').primary();
-//       user.string('username', 255);
-//       user.string('salt', 255);
-//       user.string('password', 255);
-//       user.timestamps();
-//     }).then(function(table) {
-//       console.log('Created User Table!!!!', table);
-//     });
-//   }
-// });
-
 module.exports = db;
